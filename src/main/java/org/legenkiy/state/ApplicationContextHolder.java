db143058b7b0cb931e6e1d6a2e6793b972385303
Костя Легенький <kostyantim.l05@gmail.com>
2026-02-23 22:21:22 +0200

    Now application context saves buffered  reader.

--- src/main/java/org/legenkiy/state/ApplicationContextHolder.java
+++ src/main/java/org/legenkiy/state/ApplicationContextHolder.java
@@ -3,7 +3,9 @@ package org.legenkiy.state;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
@@ -21,10 +23,12 @@ public class ApplicationContextHolder {
     private Socket socket;
     @Getter
     private PrintWriter printWriter;
-
+    @Getter
+    private BufferedReader bufferedReader;
     {
         this.socket = new Socket("localhost", 1010);
         this.printWriter = new PrintWriter(socket.getOutputStream());
+        this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
     }
 
     private ApplicationContextHolder() throws IOException {
