1e059c6a960fbf7569f8d48a0d0e1cf7b2a7dae5
Костя Легенький <kostyantim.l05@gmail.com>
2026-02-24 00:20:45 +0200

    Added start chat.

--- src/main/java/org/legenkiy/api/ChatService.java
+++ src/main/java/org/legenkiy/api/ChatService.java
@@ -4,8 +4,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 
 public interface ChatService {
 
-    void connectToChat(String username) throws JsonProcessingException;
-
-    void sendMessage(String message) throws JsonProcessingException;
+    void startChat() ;
 
 }
--- src/main/java/org/legenkiy/service/ChatServiceImpl.java
+++ src/main/java/org/legenkiy/service/ChatServiceImpl.java
@@ -1,22 +1,33 @@
 package org.legenkiy.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import org.legenkiy.api.ApplicationContextService;
 import org.legenkiy.api.ChatService;
+import org.legenkiy.api.SenderService;
 import org.legenkiy.protocol.mapper.JsonCodec;
+import org.legenkiy.protocol.message.ClientMessage;
+
+import java.util.Scanner;
 
 
 public class ChatServiceImpl implements ChatService {
 
     private final JsonCodec mapper = new JsonCodec();
-    private final ApplicationContextService applicationContextService = new ApplicationContextServiceImpl();
+    private final SenderService senderService = new SenderServiceImpl();
+    private final Scanner scanner = new Scanner(System.in);
 
     @Override
-    public void connectToChat(String username) {
-    }
+    public void startChat() {
+        System.out.println("> Enter username");
+        String username = scanner.nextLine();
+        System.out.println("> Chat with " + username  + " started");
+        System.out.println("> Enter message. If you want to end chat write /end");
+        String message;
+        while (!(message = scanner.nextLine()).equals("/end")){
+             senderService.send(
+                     ClientMessage.message(username, message)
+             );
+        }
 
-    @Override
-    public void sendMessage(String message) {
+
     }
 
 
